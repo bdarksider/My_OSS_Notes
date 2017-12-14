dfb0b44b028c7f00121293e19950bed31f5e240f
Bejoy Mathew <bejoygm@gmail.com>
2017-12-15 00:06:37 +0530

    implemented stack

--- Udacity_Technical_Interview/stack.py
+++ Udacity_Technical_Interview/stack.py
@@ -26,23 +26,27 @@ class LinkedList(object):
 
     def insert_first(self, new_element):
         "Insert new element as the head of the LinkedList"
-        pass
+        new_element.next = self.head
+        self.head = new_element
 
     def delete_first(self):
         "Delete the first (head) element in the LinkedList as return it"
-        pass
+        deleted = self.head
+        if self.head:
+            self.head = self.head.next
+            deleted.next = None
+        return deleted
+
 
 class Stack(object):
     def __init__(self,top=None):
         self.ll = LinkedList(top)
 
     def push(self, new_element):
-        "Push (add) a new element onto the top of the stack"
-        pass
+        self.ll.insert_first(new_element)
 
     def pop(self):
-        "Pop (remove) the first element off the top of the stack and return it"
-        pass
+        return self.ll.delete_first()
     
 # Test cases
 # Set up some Elements
@@ -57,7 +61,7 @@ stack = Stack(e1)
 # Test stack functionality
 stack.push(e2)
 stack.push(e3)
-print stack.pop().value
+print stack.pop()
 print stack.pop().value
 print stack.pop().value
 print stack.pop()
